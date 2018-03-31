ca4a92b618bc86741a8fe9e875d2706c798e158b
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-03-31 15:07:40 +0800

    Update Othello.java

--- Othello.java
+++ Othello.java
@@ -1,11 +1,32 @@
 package game;
-import java.util.*;
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class Othello extends JFrame{
 	public static void main(String[] args) {
 		Othello othello=new Othello();
-		State state=new State();
-		state.ff();
+		othello.showframe();
+	}
+	public void showframe() {
+		//game interface
+		JPanel gamejp=new JPanel();
+		//game button
+		JPanel buttonjp=new JPanel();
+		
+		//Button
+		ImageIcon aIcon=new ImageIcon("https://imgur.com/c8pZXKt");
+		
+		ImageIcon bIcon=new ImageIcon("https://imgur.com/2hjmbJk");
+		
+		JButton button=new JButton(aIcon);
+		button.setRolloverIcon(bIcon);
+		button.setPressedIcon(bIcon);
+		Dimension sz=new Dimension(1000, 1000);
+		button.setPreferredSize(sz);
+		this.add(gamejp);
+		this.setTitle("Othello");
+		this.setResizable(true);
+		this.setSize(1000, 800);
+		this.setVisible(true);
 	}
 }
