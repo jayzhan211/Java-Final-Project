8a03aa42100844321ea1959ae836e51d7f6dbff1
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-03-31 16:21:06 +0800

    Update Othello.java

--- Othello.java
+++ Othello.java
@@ -1,6 +1,7 @@
 package game;
 import javax.swing.*;
 import java.awt.*;
+import java.net.*;
 
 public class Othello extends JFrame{
 	public static void main(String[] args) {
@@ -14,19 +15,33 @@ public class Othello extends JFrame{
 		JPanel buttonjp=new JPanel();
 		
 		//Button
-		ImageIcon aIcon=new ImageIcon("https://imgur.com/c8pZXKt");
 		
-		ImageIcon bIcon=new ImageIcon("https://imgur.com/2hjmbJk");
 		
+		ImageIcon aIcon=new ImageIcon("images/TK.jpg");
+		ImageIcon bIcon=new ImageIcon("images/TK.jpg");
 		JButton button=new JButton(aIcon);
 		button.setRolloverIcon(bIcon);
 		button.setPressedIcon(bIcon);
 		Dimension sz=new Dimension(1000, 1000);
 		button.setPreferredSize(sz);
+		//New start
+		ImageIcon img=new ImageIcon("images/TK.jpg");
+		JButton startbutton=new JButton(img);
+		startbutton.setPreferredSize(new Dimension(100, 400));
+		buttonjp.add(button);
+		buttonjp.add(startbutton);
+		
+		this.setLayout(new GridLayout(1,2,100, 0));  
 		this.add(gamejp);
+		this.add(buttonjp);
 		this.setTitle("Othello");
 		this.setResizable(true);
-		this.setSize(1000, 800);
+		this.setSize(1000,650);
+		this.setLocationRelativeTo(null); 
+		this.setDefaultCloseOperation(3);  
 		this.setVisible(true);
+		
+		
+		
 	}
 }
