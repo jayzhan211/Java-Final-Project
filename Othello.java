9552ed82332b77e3dd5b34ff57f8d0906aa51f9e
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-04-02 22:45:40 +0800

    Update Othello.java

--- Othello.java
+++ Othello.java
@@ -5,31 +5,35 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Othello extends Application{
 	private StackPane root;
-	private Gamecontroll gc;
 	private Button startbutton;
+	private Board newgame;
 	
 	public void init() {
+		
+		
+		root=new StackPane();
+		root.getChildren().add(new Gamecontroll());
 		startbutton=new Button();
-		startbutton.setText("Start");
-		startbutton.setOnAction(new EventHandler<ActionEvent>() {
+		startbutton.setText("START");
 
+		startbutton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				System.out.println("Hello JavaFX!");
+				newgame.resetGame();
 			}
 			
 			
 		});
-		root=new StackPane();
-		gc=new Gamecontroll();
-		root.getChildren().add(gc);
 		root.getChildren().add(startbutton);
 	}
+
 	@Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Othello");
