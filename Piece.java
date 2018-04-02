93b0c5dc8a251fa6907082c027d4c07807ea57c0
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-04-02 18:12:57 +0800

    Update and rename State.java to Piece.java

---
+++ Piece.java
@@ -0,0 +1,31 @@
+package game;
+
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.transform.Translate;
+
+//Piece
+public class Piece extends Group{
+	private int player;		// the player that this piece belongs to
+    private Ellipse piece;	// ellipse representing the player's piece
+    private Translate t;
+	
+	public Piece (int player) {
+		t=new Translate();
+		piece=new Ellipse();
+		piece.getTransforms().add(t);
+		setpiece(player);
+	}
+	public void setpiece(int player) {
+		if(player==-1) {
+			piece.setFill(Color.TRANSPARENT);
+		}
+		else if(player==0) {
+			piece.setFill(Color.WHITE);
+		}
+		else {
+			piece.setFill(Color.BLACK);
+		}
+	}
+}
