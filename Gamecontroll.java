008093819a4319c7ed9540fb26d944139a619919
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-04-02 18:13:54 +0800

    Create Gamecontroll.java

---
+++ Gamecontroll.java
@@ -0,0 +1,18 @@
+package game;
+
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
+public class Gamecontroll extends Control{
+	private  Board board;
+	public Gamecontroll(){
+		board=new Board();
+		getChildren().add(board);
+		
+	}
+	
+}
