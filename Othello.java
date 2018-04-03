8b4df6d9d5051b51a9cef2e3f999440baa3ab6fc
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-04-03 12:10:57 +0800

    Update Othello.java

--- Othello.java
+++ Othello.java
@@ -3,45 +3,38 @@ package game;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Othello extends Application{
-	private StackPane root;
-	private Button startbutton;
-	private Board newgame;
+	
+	private StackPane stackPane;
+	private Gamecontroll gamecontroll;
+	
 	
 	public void init() {
-		
-		
-		root=new StackPane();
-		root.getChildren().add(new Gamecontroll());
-		startbutton=new Button();
-		startbutton.setText("START");
-
-		startbutton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				newgame.resetGame();
-			}
-			
-			
-		});
-		root.getChildren().add(startbutton);
+		stackPane = new StackPane();
+		gamecontroll=new Gamecontroll();
+		stackPane.getChildren().add(gamecontroll);
 	}
 
 	@Override
     public void start(Stage primaryStage) throws Exception {
+		
         primaryStage.setTitle("Othello");
-        primaryStage.setScene(new Scene(root, 800, 800));
+        primaryStage.setScene(new Scene(stackPane, 800, 800));
         primaryStage.setResizable(true);
         primaryStage.show();
         
     }
+	
     public static void main(String[] args) {
         launch(args);
     }
