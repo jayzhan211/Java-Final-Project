3d2b733dfb18dcf2d763e1116d455708d0bee550
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-04-03 12:22:44 +0800

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,8 +1,11 @@
 package game;
 
-import org.omg.CORBA.PRIVATE_MEMBER;
 
+
+
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
@@ -10,19 +13,39 @@ import javafx.scene.transform.Translate;
 
 //Black 2- White 1
 public class Board extends Pane{
+	
+	private Rectangle background;
+	private int currentPlayer;
+	private int nextPlayer;
+	private Piece [][] board;
+	private boolean in_play;
+	private double cell_width;
+    private double cell_height;
+    private Line[] horizontal;
+    private Line[] vertical;
+    private Translate[] horizontal_t;
+    private Translate[] vertical_t;
+    
+    
 	public Board(){
-		//black-first
 		currentPlayer=2;
 		nextPlayer=1;
 		board=new Piece[8][8];
 		background=new Rectangle();
 		horizontal = new Line[8];
-        horizontal_t = new Translate[8];
+        horizontal_t = new Translate[8];	
         vertical = new Line[8];
         vertical_t = new Translate[8];
+        
+        
+        
+        initbackground();
+        initboard();
+        resetGame();
 		
 	}
 	public void resetGame() {
+		//System.out.println("123");
 		in_play=true;
 		reset_board();
 		//white
@@ -37,7 +60,7 @@ public class Board extends Pane{
 		nextPlayer=1;
 		
 	}
-	public void reset_board() {
+	private void reset_board() {
 		
 		for(int i=0;i<8;i++)
 			for(int j=0;j<8;j++)
@@ -47,6 +70,39 @@ public class Board extends Pane{
 	public void placePiece(double x,double y) {
 		
 		
+	}
+	private void initbackground() {
+		background.setFill(Color.SILVER);
+        getChildren().addAll(background);
+        for(int i = 0; i < 8; i++) {
+            horizontal[i] = new Line();
+            horizontal[i].setStroke(Color.WHITE);
+            horizontal[i].setStartX(0);
+            horizontal[i].setStartY(0);
+            horizontal[i].setEndY(0);
+            horizontal_t[i] = new Translate(0,0);
+            horizontal[i].getTransforms().add(horizontal_t[i]);
+            getChildren().add(horizontal[i]);
+        }
+
+        for(int i = 0; i < 8; i++) {
+            vertical[i] = new Line();
+            vertical[i].setStroke(Color.WHITE);
+            vertical[i].setStartX(0);
+            vertical[i].setStartY(0);
+            vertical[i].setEndX(0);
+            vertical_t[i] = new Translate(0,0);
+            vertical[i].getTransforms().add(vertical_t[i]);
+            getChildren().add(vertical[i]);
+        }
+	}
+	private void initboard() {
+		for(int i=0;i<8;i++)
+			for(int j=0;j<8;j++) {
+				board[i][j]=new Piece(0);
+				getChildren().add(board[i][j]);
+			}
+					
 	}
 	public void resize(double width,double height) {
 		super.resize(width,height);
@@ -70,16 +126,4 @@ public class Board extends Pane{
             vertical[i].setEndY(height);
         }
     }
-	
-	private Rectangle background;
-	private int currentPlayer;
-	private int nextPlayer;
-	private Piece [][] board;
-	private boolean in_play;
-	private double cell_width;
-    private double cell_height;
-    private Line[] horizontal;
-    private Line[] vertical;
-    private Translate[] horizontal_t;
-    private Translate[] vertical_t;
 }
