5ca1ba395e21396cf0b7953c28c56adb3bed9186
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-04-03 12:10:37 +0800

    Update Gamecontroll.java

--- Gamecontroll.java
+++ Gamecontroll.java
@@ -9,18 +9,38 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 public class Gamecontroll extends Control{
-	private Board board;
+	private final Board board;
 	
-	
+		
 	public Gamecontroll(){
-		
+		setSkin(new Boardskin(this));
 		board=new Board();
-		
 		getChildren().add(board);
 		
+		setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+            	
+                board.placePiece(event.getX(), event.getY());
+            }
+        });
+
+        // TODO: add press handler which calls reset when space is clicked - pt 4
+        setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if(event.getCode() == KeyCode.SPACE) {
+                	
+                    board.resetGame();
+                }
+            }
+        });
 
 		
 	}
+	public void resize(double width, double height) {
+        super.resize(width, height);
+        board.resize(width, height);
+    }
 	
 }
-
