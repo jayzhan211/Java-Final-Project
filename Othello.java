53f2e949f4d0673114f1476d5b20ad4f0d931df8
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-04-08 10:52:18 +0800

    Add files via upload

--- Othello.java
+++ Othello.java
@@ -23,51 +23,94 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
 
 public class Othello extends Application{
 	
 	private StackPane start_pane;
-	private StackPane main_pane;
+	private StackPane pvp_pane;
+	private StackPane choose_pane;
+	private StackPane pvc_pane;
+	
 	private Gamecontroll gamecontroll;
 	private Button startButton;
 	private Label game_title;
-
+	private Button return_button;
+	//player vs player
+	private Button go_to_pvp;
+	//player vs computer
+	private Button go_to_pvc;
+	
 	private Scene start_scene;
-	private Scene main_scene;
+	private Scene choose_scene;
+	private Scene pvp_scene;
+	private Scene pvc_scene;
+	
+	private int window_height=800;
+	private int window_width=800;
 	
 	public void init() {
 		//main pane
 		start_pane = new StackPane();
-		main_pane=new StackPane();
-		
-		startButton = new Button("Start");
-		
-		game_title=new Label("Othello");
-		
-		
+		pvp_pane=new StackPane();
+		pvc_pane=new StackPane();
+		choose_pane=new StackPane();
 		gamecontroll=new Gamecontroll();
 		
-		//stackpane ,note that  button should be on top
-		start_pane.getChildren().add(game_title);
-		start_pane.getChildren().add(startButton);
+		pvp_pane.getChildren().add(gamecontroll);
 		
+		//pvc_pane.getStylesheets().add(getClass().getResource("pvc_paneskin.css").toExternalForm());
+		
+		pvc_pane.getStyleClass().add("pvc_skin");
+
+		
+		startButton = new Button("Start");
+		return_button=new Button("Return");
+		go_to_pvp=new Button("Player vs Player");
+		go_to_pvc=new Button("Player vs Computer");
+		
+		game_title=new Label("Othello");
 		
 		game_title.setMaxHeight(500);
 		game_title.setFont(new Font("Fleisch",100));
 		game_title.setAlignment(Pos.TOP_CENTER);
 		game_title.setTextFill(Color.BLACK);
 		
-		startButton.setFont(new Font("Fleisch",14));
+		//stackpane ,note that  button should be on top
+		start_pane.getChildren().add(game_title);
+		start_pane.getChildren().add(startButton);
 		
+		
+
+		return_button.setTranslateX(-320);
+		return_button.setTranslateY(-360);
+		return_button.setScaleX(1.3);
+		
+
+		
+		go_to_pvp.setTranslateX(0);
+		go_to_pvp.setTranslateY(-50);
+		
+		
+		
+		startButton.setFont(new Font("Fleisch",14));
 		startButton.setMaxSize(60, 40);
 		
-		main_pane.getChildren().add(gamecontroll);
 		
-		main_scene=new Scene(main_pane, 800, 800);
-        start_scene=new Scene(start_pane,800,800);
 		
+		choose_pane.getChildren().addAll(return_button,go_to_pvc,go_to_pvp);
+		
+		pvc_scene=new Scene(pvc_pane,window_width, window_height);
+		pvp_scene=new Scene(pvp_pane,window_width, window_height);
+        start_scene=new Scene(start_pane,window_width, window_height);
+		choose_scene=new Scene(choose_pane,window_width, window_height);
+		
+		//pvc_paneskin.css
+		
+		pvc_scene.getStylesheets().add(getClass().getResource("skin.css").toExternalForm());
 		
 	}
 
@@ -77,10 +120,21 @@ public class Othello extends Application{
         primaryStage.setTitle("Othello");
         primaryStage.setScene(start_scene);
         startButton.setOnAction(e->{
-        	primaryStage.setScene(main_scene);
+        	primaryStage.setScene(choose_scene);
 			
 		});
-        
+        return_button.setOnAction(e->{
+        	primaryStage.setScene(start_scene);
+			
+		});
+        go_to_pvp.setOnAction(e->{
+        	primaryStage.setScene(pvp_scene);
+			
+		});
+        go_to_pvc.setOnAction(e->{
+        	primaryStage.setScene(pvc_scene);
+			
+		});
         
         primaryStage.setResizable(true);
 
