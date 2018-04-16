c81140bbe61147144c5ac72e409c345943f38bc6
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-04-16 21:39:50 +0800

    Update Board_PVC.java

--- Board_PVC.java
+++ Board_PVC.java
@@ -9,26 +9,30 @@ import javafx.animation.SequentialTransition;
 import javafx.animation.Timeline;
 import javafx.scene.transform.Rotate;
 
-public class Board_PvC extends Board{
+public class Board_PvC extends Board_Beta{
+	public Board_PvC(int Board_Size) {
+		super(Board_Size);
+
+	}
 	public void placePiece(double x,double y) {
-		cellx=(int) (x / cell_width);
-		celly = (int) (y / cell_height);
-		
+		final int cellx = (int) (x / Board_Size);
+        final int celly = (int) (y / Board_Size);
+
         //EndGmae
         if(!in_play)return ;
         //Have Piece
         if(!in_board(cellx, cellx))return;
-        if(board[cellx][celly].getpiece()!=0) return ;
-        
+        if(board[cellx][celly].getType()!=PieceType.NONE) return ;
+
         for(int i=0;i<8;i++)
         	for(int j=0;j<8;j++)
         		can_verse[i][j]=false;
         //player can move
         if(determinevalidmove(cellx,celly)) {
         	cur_state++;
-        	board[cellx][celly].setpiece(currentPlayer);
+        	board[cellx][celly].setType(currentPlayer);
         	flip(cellx,celly);
-        	
+
         }
         swapPlayer();
         updatescore();
@@ -36,10 +40,10 @@ public class Board_PvC extends Board{
         if(!in_play)findWinner();
         for(int i=0;i<8;i++)
 			for(int j=0;j<8;j++)
-				board_state[cur_state][i][j].setpiece(board[i][j].getpiece());
+				board_state[cur_state][i][j].setType(board[i][j].getType());
         computer_turn();
-       
-        
+
+
 	}
 	private void computer_turn() {
 		Timeline timeline=new Timeline();
