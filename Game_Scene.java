7d594f42d100d77689e07834c09441b9af90db85
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-04-29 15:17:51 +0800

    Update Game_Scene.java

--- Game_Scene.java
+++ Game_Scene.java
@@ -2,9 +2,14 @@ package game;
 
 import java.util.Collection;
 
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
+import javafx.animation.SequentialTransition;
 import javafx.geometry.Point2D;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.transform.Rotate;
+import javafx.util.Duration;
 
 public class Game_Scene extends StackPane{
 	public BoardUI board_state;
@@ -13,6 +18,9 @@ public class Game_Scene extends StackPane{
 	public Label showturn;
 	public static Player human = Player.BLACK;
 	public boolean vsRobots = true;
+	public Duration flipDuration;
+    public int flip_duration=500;
+
 	public Game_Scene() {
 		board_state=new BoardUI();
 		whitescore=new Label("White");
@@ -22,36 +30,47 @@ public class Game_Scene extends StackPane{
 		whitescore.setTranslateY(300);
 		blackscore.setTranslateX(-250);
 		blackscore.setTranslateY(300);
-		this.getChildren().addAll(board_state,whitescore,blackscore);
-
-
-
+		showturn.setTranslateX(0);
+		showturn.setTranslateY(300);
+		this.getChildren().addAll(board_state,whitescore,blackscore,showturn);
+		this.flipDuration=Duration.millis(flip_duration);
 	}
 	public void updateScore(int blackscore, int whitescore) {
 		this.blackscore.setText("Black: " + blackscore);
 		this.whitescore.setText("Black: " + whitescore);
 	}
 	public void declareDraw() {
-		//this.showTurn.setFont(showTurn.getFont().deriveFont(Font.BOLD));
 		this.showturn.setText("Draw !?");
 	}
 	public void declareWinner(String winnerName) {
-		//this.showTurn.setFont(showTurn.getFont().deriveFont(Font.BOLD));
 		this.showturn.setText(winnerName + "!");
 	}
 	public void markPossibleMoves(Collection<Point2D> possibleMoves, SquareType color) {
-		System.out.println("bbb1");
-		for (Point2D possiblePoint : possibleMoves) {
-			//System.out.println(possiblePoint.getX()+" "+possiblePoint.getY());
+		for (Point2D possiblePoint : possibleMoves) 
 			setSquare(possiblePoint, color);
-		}
-		System.out.println("bbb2");
 	}
 	private void setSquare(Point2D point, SquareType squareType) {
 		int x=(int)point.getX();
 		int y=(int)point.getY();
-		board_state.board[x][y].setType(squareType);
-
+		
+		if(squareType==SquareType.BLACK||squareType==SquareType.WHITE) {
+			RotateTransition firstRotator = new RotateTransition(flipDuration,board_state.board[x][y]);
+			firstRotator.setAxis(Rotate.Y_AXIS);
+			firstRotator.setFromAngle(0);
+			firstRotator.setToAngle(90);
+			firstRotator.setInterpolator(Interpolator.LINEAR);
+			firstRotator.setOnFinished(e->{
+				board_state.board[x][y].setType(squareType);
+	    	});
+		    RotateTransition secondRotator = new RotateTransition(flipDuration,board_state.board[x][y]);
+		    secondRotator.setAxis(Rotate.Y_AXIS);
+		    secondRotator.setFromAngle(90);
+		    secondRotator.setToAngle(180);
+		    secondRotator.setInterpolator(Interpolator.LINEAR);
+		    new SequentialTransition(firstRotator, secondRotator).play();
+	    }
+		else board_state.board[x][y].setType(squareType);
+		
 	}
 	public void unmarkPossibleMoves(Collection<Point2D> possibleMoves) {
 
