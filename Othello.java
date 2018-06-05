ff96005b77e22f9f4309b0a437e9d8686ee02aa2
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-06-05 16:46:13 +0800

    Add files via upload

--- Othello.java
+++ Othello.java
@@ -3,11 +3,9 @@ package game;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
-
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
