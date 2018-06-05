28d022d74411ab0630def076ef47e5634c5e6cb5
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-06-05 16:44:07 +0800

    Update NegaMax.java

--- NegaMax.java
+++ NegaMax.java
@@ -2,19 +2,19 @@ package game;
 
 
 import javafx.geometry.Point2D;
+
 import java.util.Set;
-
 public class NegaMax{
-
 	public SearchResult search(final Board board, final Player player, int alpha, int beta, final int depth, final ScoreEval evfunction) {
-		if (depth <= 0 || isEndState(board)) 
+
+		if (depth <= 0 || isEndState(board))
 			return new SearchResult(null, evfunction.evaluate(board, player));
-		else { 
+		else {
 			Set<Point2D> possibleMoves = MoveExplorer.explore(board, player.color());
 			SearchResult best = new SearchResult(null, alpha);
-			if (possibleMoves.isEmpty()) { /* turn is lost - check next player */
+			if (possibleMoves.isEmpty()) { //no move ,pass
 				possibleMoves = MoveExplorer.explore(board, player.opponent().color());
-				if (possibleMoves.isEmpty()) { /* end of game - is there a winner ? */
+				if (possibleMoves.isEmpty()) { //game end
 					switch (Integer.signum(board.count(player.color()) - board.count(player.opponent().color()))) {
 						case -1:
 							best = new SearchResult(null, Integer.MIN_VALUE);
@@ -27,23 +27,22 @@ public class NegaMax{
 							break;
 					}
 				}
-				else { /* game continues - no moves to check */
+				else
 					best = search(board, player.opponent(), -beta, -alpha, depth - 1, evfunction).negated();
-				}
+
 			}
-			else{ /* check the score of each move */
+			else {
 				for (Point2D nextPossibleMove : possibleMoves) {
 					Board subBoard = board.clone();
 					subBoard.makeMove(nextPossibleMove, player.color());
 					int score = search(subBoard, player.opponent(), -beta, -alpha, depth - 1, evfunction).negated().getScore();
-					if (alpha < score) {
+					if (alpha < score) { //update the range
 						alpha = score;
 						best = new SearchResult(nextPossibleMove, score);
 					}
-					/* Alpha Beta Pruning */
-					if (alpha >= beta) {
+					if (alpha >= beta) //Pruning
 						return best;
-					}
+
 				}
 			}
 			return best;
