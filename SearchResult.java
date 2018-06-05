6b1a956090bf229101e98749f2966c78f238f69a
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-06-05 16:44:53 +0800

    Update SearchResult.java

--- SearchResult.java
+++ SearchResult.java
@@ -1,24 +1,17 @@
 package game;
-
 import javafx.geometry.Point2D;
-
 public class SearchResult {
-
 	private Point2D point;
 	private int score;
-
 	public int getScore() {
 		return score;
 	}
-
 	public Point2D getPoint() {
 		return point;
 	}
-
 	public SearchResult negated() {
 		return new SearchResult(point, -score);
 	}
-
 	public SearchResult(Point2D nextPossibleMove, int score) {
 		this.point = nextPossibleMove;
 		this.score = score;
