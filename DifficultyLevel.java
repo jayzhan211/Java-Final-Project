27353a90763c00fa810709c60397389c40f5df43
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-06-15 15:21:06 +0800

    Update DifficultyLevel.java

--- DifficultyLevel.java
+++ DifficultyLevel.java
@@ -1,9 +1,9 @@
 package game;
 public enum DifficultyLevel {
-	EASY(3),
+	EASY(4),
 	NORMAL(5),
-	HARD(7),
-	Nightmare(9);
+	HARD(6),
+	Nightmare(7);
 	public int level;
 	private DifficultyLevel(int level) {
 		this.level = level;
