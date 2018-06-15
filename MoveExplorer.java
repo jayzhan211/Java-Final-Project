c61f52f7343d1953440b30194b5275a53b57fcc5
ジブリール-Jiburiru <jay70550@yahoo.com.tw>
2018-06-15 15:27:27 +0800

    Update MoveExplorer.java

--- MoveExplorer.java
+++ MoveExplorer.java
@@ -9,9 +9,9 @@ import javafx.geometry.Point2D;
 public class MoveExplorer {
 
 
-	private static boolean shouldSearch(final Board board, final Point2D seed, final Direction direction) {
-		Point2D nextPoint = direction.next(seed);
-		return pointIsValid(nextPoint) ? board.getSquareState(nextPoint) == board.getSquareState(seed).opposite() : false;
+	private static boolean shouldSearch(final Board board, final Point2D cur, final Direction direction) {
+		Point2D nextPoint = direction.next(cur);
+		return pointIsValid(nextPoint) ? board.getSquareState(nextPoint) == board.getSquareState(cur).opposite() : false;
 	}
 	private static boolean pointIsValid(Point2D point) {
 		return point.getX() >= 0 && point.getX() < Board.BOARD_LENGTH
@@ -21,15 +21,16 @@ public class MoveExplorer {
 	public static Set<Point2D> explore(final Board board, final SquareState state) {
 		Set<Point2D> possibleMoves = new HashSet<Point2D>();
 		Set<Point2D> statePoints = board.getSquares(state);
-		for (Point2D seed : statePoints) {
+		for (Point2D cur : statePoints) {
 			for (Direction direction : Direction.values()) {
-				if (shouldSearch(board, seed, direction)) {
-					Point2D nextPoint = direction.next(seed);
+				if (shouldSearch(board, cur, direction)) {
+					Point2D nextPoint = direction.next(cur);
 					nextPoint = direction.next(nextPoint);
 					while (pointIsValid(nextPoint)) {
 						if (board.getSquareState(nextPoint) == state) {
 							break;
-						} else if (board.getSquareState(nextPoint) == SquareState.EMPTY) {
+						}
+						else if (board.getSquareState(nextPoint) == SquareState.EMPTY) {
 							possibleMoves.add(nextPoint);
 							break;
 						}
@@ -42,16 +43,16 @@ public class MoveExplorer {
 	}
 	public static Set<Point2D> squaresToFill(final Board board, final Point2D seed) {
 		Set<Point2D> filledlist = new HashSet<Point2D>();
-		SquareState seedState = board.getSquareState(seed);
+		SquareState cur_state = board.getSquareState(seed);
 		for (Direction direction : Direction.values()) {
 			if (shouldSearch(board, seed, direction)) {
 				Point2D nextPoint = direction.next(seed);
 				LinkedList<Point2D> templist = new LinkedList<Point2D>();
 				while (pointIsValid(nextPoint)) {
 					SquareState nextState = board.getSquareState(nextPoint);
-					if (nextState == seedState.opposite()) {
+					if (nextState == cur_state.opposite()) {
 						templist.add(nextPoint);
-					} else if (nextState == seedState) {
+					} else if (nextState == cur_state) {
 						filledlist.addAll(templist);
 						break;
 					} else if (nextState == SquareState.EMPTY) {
